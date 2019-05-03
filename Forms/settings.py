87e8b0b785697f8cd302bfcfa583509b371f6dc8
blackkeyboard141 <rasik.hasan@yahoo.com>
2019-05-04 02:27:05 +0600

    update ssshhh

--- Forms/settings.py
+++ Forms/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6n#wfxke*corh7ygmz+@otn5dpoac__-(akr$o)61sk26ph*r&'
+# SECRET_KEY = '6n#wfxke*corh7ygmz+@otn5dpoac__-(akr$o)61sk26ph*r&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psionicassessment.herokuapp.com']
 
 
 # Application definition
